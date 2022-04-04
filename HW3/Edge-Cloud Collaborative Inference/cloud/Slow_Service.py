7ce9c543010c33e247f52705586bd1bfd72f4295
Mustafa Omer Mohammed Elamin Elshaigi <81756320+Mustafa-omer-mohammed@users.noreply.github.com>
2022-04-04 16:21:08 +0200

    Preprocessing pipeline
    Developing the preprocessing pipeline to transform the raw audio to MFCC spectrogram  matrix

--- HW3/Edge-Cloud Collaborative Inference/cloud/Slow_Service.py
+++ HW3/Edge-Cloud Collaborative Inference/cloud/Slow_Service.py
@@ -29,6 +29,41 @@ labels =  tf.convert_to_tensor(labels)
 ############ Create the Keywords Spotting Class KWS ######################3
 class  KWS(object):
     exposed = True
+    def __init__(self):
+        self.sampling_rate = 16000                              # 16000  
+        self.frame_length = 640                                               # 640 
+        self.frame_step = 320                                                   # 320 
+        self.num_mel_bins = 40                                               # 40 
+        self.lower_frequency = 20                                         # 20 
+        self.upper_frequency = 4000                                         # 4000
+        self.num_coefficients = 10 										# 10 
+        num_spectrogram_bins = self.frame_length // 2 + 1
+
+        self.linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
+						self.num_mel_bins, num_spectrogram_bins, self.sampling_rate, 20, 4000)
+
+    def preprocess(self ,audio_string):
+        # decode and normalize
+        audio_bytes = base64.b64decode(audio_string)
+        audio, _ = tf.audio.decode_wav(audio_bytes)
+        audio = tf.squeeze(audio, axis=1)
+        # Padding for files with less than 16000 samples
+        zero_padding = tf.zeros([self.sampling_rate] - tf.shape(audio), dtype=tf.float32)   
+        audio = tf.concat([audio, zero_padding], 0)
+        audio.set_shape([self.sampling_rate])
+
+        stft = tf.signal.stft(audio, frame_length=self.frame_length,frame_step=self.frame_step, fft_length=self.frame_length)
+        spectrogram = tf.abs(stft)
+
+        mel_spectrogram = tf.tensordot(spectrogram, self.linear_to_mel_weight_matrix, 1)
+        log_mel_spectrogram = tf.math.log(mel_spectrogram + 1.e-6)
+        mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
+        mfccs = mfccs[..., :self.num_coefficients]
+
+        mfccs = tf.expand_dims(mfccs, -1)
+        mfccs = tf.expand_dims(mfccs, 0)
+
+        return mfccs  
 
     def GET(self, *path, **query):
         pass
